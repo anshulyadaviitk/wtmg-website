eaf7bd24ee600042fb9e81a23940c6d770283809
anshulyadaviitk <anshulyadav.iitk@gmail.com>
2025-06-17 09:55:12 +0530

    Update collaborations.js

--- src/content/collaborations.js
+++ src/content/collaborations.js
@@ -1,19 +1,19 @@
 export const National = [
   {
     name: "Indian Institute of Science, Bengaluru, India",
-    logo: "/images/collaborations/logo_national/iisc.jpg",
+    logo: "/images/collaborations/logo_national/iisc_bangalore.jpg",
   },
   {
     name: "Indian Institute of Technology, Guwahati, India",
-    logo: "/images/collaborations/logo_national/IITG_logo.png",
+    logo: "/images/collaborations/logo_national/iit_guwahati.png",
   },
   {
     name: "National Institute of Hydrology, Roorkee, India",
-    logo: "/images/collaborations/logo_national/im13.jpeg",
+    logo: "/images/collaborations/logo_national/nih_roorkee.jpeg",
   },
   {
     name: "Indian Institute of Technology, Kanpur, India",
-    logo: "/images/collaborations/logo_national/iitkanpur.jpg",
+    logo: "/images/collaborations/logo_national/iit_kanpur.jpg",
   },
   {
     name: "National Institute of Technology, Warangal, India",
@@ -25,27 +25,25 @@ export const National = [
   },
   {
     name: "Kamla Nehru Institute of Technology, Sultanpur, India",
-    logo: "/images/collaborations/logo_national/knit.jpg",
+    logo: "/images/collaborations/logo_national/knit_sultanpur.jpg",
   },
   {
     name: "National Institute of Technology, Uttarakhand, India",
     logo: "/images/collaborations/logo_national/nit_uttarakhand.png",
   },
   {
-    name: "CSIR - Central Salt and Marine Chemicals Research Institute, Bhavnagar, India",
-    logo: "/images/collaborations/logo_national/csir.png",
+    name: "CSIR-Central Salt and Marine Chemicals Research Institute, Bhavnagar, India",
+    logo: "/images/collaborations/logo_national/csir_csmcri.png",
   },
   {
-    name: "CSIR - Structural Engineering Research Centre, Chennai, India",
+    name: "CSIR-Structural Engineering Research Centre, Chennai, India",
     logo: "/images/collaborations/logo_national/csir_serc.jpg",
   },
   {
-    name: "Gujarat Forestry Science Centre (GFSC), India",
-    logo: "/images/collaborations/logo_national/gfsc_gujarat.png",
+    name: "GSFC University, India",
+    logo: "/images/collaborations/logo_national/gsfc_gujarat.png",
   },
 ];
-
-
 
 export const International = [
   {
